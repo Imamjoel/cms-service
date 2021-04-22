34444edb8ac63fc536935b9e69cfa285a84efa87
Imam Fahrizal <mfahreinheitz@gmail.com>
2021-04-22 15:55:54 +0700

    fix cms post route

--- resources/views/cms/index.blade.php
+++ resources/views/cms/index.blade.php
@@ -10,14 +10,14 @@
                 <article class="post-item">
                 @if ($value->image_url)
                     <div class="post-item-image">
-                        <a href="{{ route('cms.show', $value->id) }}">
+                        <a href="{{ route('cms.show', $value->slug) }}">
                             <img src="{{ $value->image_url }}" alt="">
                         </a>
                     </div>
                 @endif
                     <div class="post-item-body">
                         <div class="padding-10">
-                            <h2><a href="{{ route('cms.show', $value->id) }}">{{ $value->title }}</a></h2>
+                            <h2><a href="{{ route('cms.show', $value->slug) }}">{{ $value->title }}</a></h2>
                             <p>{{ $value->excerpt }}</p>
                         </div>
 
@@ -31,7 +31,7 @@
                                 </ul>
                             </div>
                             <div class="pull-right">
-                                <a href="{{ route('cms.show', $value->id) }}">Continue Reading &raquo;</a>
+                                <a href="{{ route('cms.show', $value->slug) }}">Continue Reading &raquo;</a>
                             </div>
                         </div>
                     </div>
